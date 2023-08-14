29fe314b73ab8ea1f1a979be67ad508f7a7fcf50
Nick Burr <nicholasstclairburr@gmail.com>
2023-08-14 15:58:53 -0600

    saving my questions, also made the variables for inquirer and pulling in the other function.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {   type: 'input',
+name: 'readmeTitle',
+message: 'Please input the title of your README:',
+    },
+    {   type: 'input',
+name: 'installation',
+message: 'Please input the Installation Instructions of your README:',
+    },
+    {   type: 'input',
+name: 'usage',
+message: 'Please input your usage information:',
+    },
+    {   type: 'input',
+name: 'contribution',
+message: 'Please input your contribution guidelines:',
+    },
+    {   type: 'input',
+name: 'tests',
+message: 'Please input your tests:',
+    },  
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
