97fc0e1f203176b7335d87ea9d119822d5e069da
Nick Burr <nicholasstclairburr@gmail.com>
2023-08-21 13:49:38 -0600

    fixed bug

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,8 +4,8 @@ function renderLicenseBadge(license) {
   if (license === undefined || license === null){
     return ''
   }
-  if (license === 'MIT') return `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`
-  if (license === 'Apache') return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  if (license === 'MIT') return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  if (license === 'Apache') return `[![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
   if (license === 'GPL v3') return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
   if (license === 'GPL v2') return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
   if (license === 'BSD 3-Clause') return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
